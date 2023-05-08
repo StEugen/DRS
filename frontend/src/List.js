e305f444be5f99c696c476bc6828e157ef08d3c3
StEugen <85479518+StEugen@users.noreply.github.com>
2023-05-08 12:01:23 +0000

    Update List.js
    Made 'proper' List display, it just Table requests to backend left and Search

--- frontend/src/List.js
+++ frontend/src/List.js
@@ -2,68 +2,87 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Button, Typography, Box } from '@material-ui/core';
 import { styled } from '@mui/system';
-import Table from './Table.js'
+import Table from './Table.js';
+
+const Container = styled(Box)({
+  display: 'flex',
+});
+
+const ListContainer = styled(Box)({
+  width: '20%',
+  border: '1px solid black',
+  padding: '10px',
+  marginRight: '10px',
+  overflowY: 'scroll',
+  height: '400px',
+});
+
+
 
 export default function List(props) {
-    const [faculties, setFaculties] = useState([]);
-    const [cabinets, setCabinets] = useState({});
-    const [selectedCabinet, setSelectedCabinet] = useState("");
-    const [selectedFaculty, setSelectedFaculty] = useState("");
-    useEffect(() => {
-        axios.get('/api/facultylist/')
-            .then(response => setFaculties(response.data));
-    }, []);
+  const [faculties, setFaculties] = useState([]);
+  const [cabinets, setCabinets] = useState({});
+  const [selectedCabinet, setSelectedCabinet] = useState("");
+  const [selectedFaculty, setSelectedFaculty] = useState("");
 
-    useEffect(() => {
-        axios.get('/api/cabinetslist/')
-            .then(response => {
-                const groupedCabinets = {};
-                response.data.forEach(cabinet => {
-                    const facultyId = cabinet.faculty;
-                    const faculty = faculties.find(f => f.id === facultyId);
-                    if (faculty) {
-                        const facultyName = faculty.name;
-                        if (!(facultyName in groupedCabinets)) {
-                            groupedCabinets[facultyName] = [];
-                        }
-                        groupedCabinets[facultyName].push(cabinet);
-                    }
-                });
-                setCabinets(groupedCabinets);
-            });
-    }, [faculties]);
+  useEffect(() => {
+    axios.get('/api/facultylist/')
+      .then(response => setFaculties(response.data));
+  }, []);
 
-    return (
-      <Box>
+  useEffect(() => {
+    axios.get('/api/cabinetslist/')
+      .then(response => {
+        const groupedCabinets = {};
+        response.data.forEach(cabinet => {
+          const facultyId = cabinet.faculty;
+          const faculty = faculties.find(f => f.id === facultyId);
+          if (faculty) {
+            const facultyName = faculty.name;
+            if (!(facultyName in groupedCabinets)) {
+              groupedCabinets[facultyName] = [];
+            }
+            groupedCabinets[facultyName].push(cabinet);
+          }
+        });
+        setCabinets(groupedCabinets);
+      });
+  }, [faculties]);
+
+  return (
+    <Container>
+      <ListContainer>
         {faculties.map(faculty => (
-            <div key={faculty.id}>
-                <Typography>
-                    {faculty.name} 
-                </Typography>
-                {cabinets[faculty.name] &&
-                    cabinets[faculty.name].map(cabinet => (
-                        <Button 
-                            key={cabinet.id}
-                            onClick={() => {
-                                setSelectedCabinet(cabinet);
-                                setSelectedFaculty(faculty);
-                            }}
-                            
-                        >
-                            {cabinet.cabinet}
-                        </Button>
-                    ))
-                }
-            </div>
+          <div key={faculty.id}>
+            <Typography>
+              {faculty.name}
+            </Typography>
+            {cabinets[faculty.name] &&
+              cabinets[faculty.name].map(cabinet => (
+                <Button
+                  key={cabinet.id}
+                  onClick={() => {
+                    setSelectedCabinet(cabinet);
+                    setSelectedFaculty(faculty);
+                  }}
+                >
+                  {cabinet.cabinet}
+                </Button>
+              ))
+            }
+          </div>
         ))}
-        {setSelectedCabinet && (
-            <Box>
-                <Table 
-                    cabinet={selectedCabinet} 
-                    faculty={selectedFaculty}
-                />
-            </Box>
+      </ListContainer>
+        {selectedCabinet && (
+          <Box>
+            <Table
+              cabinet={selectedCabinet}
+              faculty={selectedFaculty}
+            />
+          </Box>
         )}
-      </Box>
-    )
+    </Container>
+  );
 }
+
+
