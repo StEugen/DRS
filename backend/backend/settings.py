3ee81b766d05d5de57191836f3fc0501f63c7124
StEugen <85479518+StEugen@users.noreply.github.com>
2023-05-09 10:33:39 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,11 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'data',
-    'admin_interface',
-    'colorfield',
 ]
-
-X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -133,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
+STATIC_ROOT = '/static/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
