5afbb868f2823dca80374fe6a265b65a420f5501
StEugen <85479518+StEugen@users.noreply.github.com>
2023-05-09 10:29:53 +0000

    colors changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'data',
+    'admin_interface',
+    'colorfield',
 ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -128,6 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
